3863900650c78f2c2738525494e52fd81d016672
iGuest <5tigerjelly@gmail.com>
2016-04-09 23:33:41 -0700

    add job, person, family

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -27,29 +27,24 @@ public struct Money {
   public var amount : Int
   public var currency : String
   
-   public init(amount: Int, currency: String){
-        self.amount = amount
-        self.currency = currency
-    }
-    
   public func convert(to: String) -> Money {
     let check = (currency, to)
-        switch check {
-            case ("USD", "GBP"): return Money(amount: amount/2, currency: "GBP")
-            case ("GBP", "USD"): return Money(amount: amount*2, currency: "USD")
-            case ("USD", "EUR"): return Money(amount: amount*15/10, currency: "EUR")
-            default: return Money(amount: amount, currency: currency)
-        }
+    switch check {
+    case ("USD", "GBP"): return Money(amount: amount/2, currency: "GBP")
+    case ("GBP", "USD"): return Money(amount: amount*2, currency: "USD")
+    case ("USD", "EUR"): return Money(amount: amount*15/10, currency: "EUR")
+    default: return Money(amount: amount, currency: currency)
     }
+  }
   
-    public func add(to: Money) -> Money {
-        if to.currency == currency {
-            return Money(amount: amount+to.amount, currency: currency)
-        }else{
-            let newmoney = self.convert(to.currency)
-            return Money(amount: newmoney.amount+to.amount, currency: currency)
-        }
+  public func add(to: Money) -> Money {
+    if to.currency == currency {
+        return Money(amount: amount+to.amount, currency: currency)
+    }else{
+        let newmoney = self.convert(to.currency)
+        return Money(amount: newmoney.amount+to.amount, currency: currency)
     }
+  }
   public func subtract(from: Money) -> Money {
     if from.currency == currency{
         return Money(amount: amount-from.amount, currency: currency)
@@ -60,83 +55,118 @@ public struct Money {
   }
 }
 
-//////////////////////////////////////
-//// Job
-////
-//public class Job {
-//    
-//    public var title : String
-//    public var salary : Int
-//    
-//    //how do you play with enum?
-//  public enum JobType {
-//    case Hourly(Double)
-//    case Salary(Int)
-//  }
-//    //very confusing !!
-//    public init(title : String, type : JobType) {
-//        self.title = title
-//        switch type {
-//        case .Hourly: JobType.Hourly = type.rawValue
-//        case .Salary:<#code#>
-//        }
-//    }
-//  
-//    public func calculateIncome(hours: Int) -> Int {
-//        return Int(JobType.Hourly(Double(hours)))
-//    }
-//  
-//  public func raise(amt : Double) {
-//  }
-//}
+////////////////////////////////////
+// Job
 //
-//////////////////////////////////////
-//// Person
-////
-//public class Person {
-//  public var firstName : String = ""
-//  public var lastName : String = ""
-//  public var age : Int = 0
+public class Job {
+    
+    public var title: String
+    public var salary: JobType
+    
+    public enum JobType {
+        case Hourly(Double)
+        case Salary(Int)
+    }
+  
+  public init(title : String, type : JobType) {
+    self.title = title
+    self.salary = type
+  }
+  
+    public func calculateIncome(hours: Int) -> Int {
+        switch salary {
+        case .Hourly:
+            let numner = salary.Hourly.rawValue
+            return Int(salary.Hourly())*hours
+        case .Salary: return salary
+        }
+    }
+  
+  public func raise(amt : Double) {
+  }
+}
+
+////////////////////////////////////
+// Person
 //
-//  public var job : Job? {
-//    get { }
-//    set(value) {
-//    }
-//  }
-//  
-//  public var spouse : Person? {
-//    get { }
-//    set(value) {
-//    }
-//  }
-//  
-//  public init(firstName : String, lastName: String, age : Int) {
-//    self.firstName = firstName
-//    self.lastName = lastName
-//    self.age = age
-//  }
-//  
-//  public func toString() -> String {
-//  }
-//}
+public class Person {
+  public var firstName : String = ""
+  public var lastName : String = ""
+  public var age : Int = 0
+
+  public var job : Job? {
+    get {
+        return self.job
+    }
+    set(value) {
+        if age < 16 {
+            self.job = Job(title: (value?.title)!, type: (value?.salary)!)
+        }else{
+            self.job = nil
+        }
+    }
+  }
+  
+  public var spouse : Person? {
+    get { }
+    set(value) {
+        if age < 18 {
+            self.spouse = Person(firstName: (value?.firstName)!, lastName: (value?.lastName)!, age: (value?.age)!)
+        }else{
+            // isn't the default nil anyway?
+            self.spouse = nil
+        }
+    }
+  }
+  
+  public init(firstName : String, lastName: String, age : Int) {
+    self.firstName = firstName
+    self.lastName = lastName
+    self.age = age
+  }
+  
+  public func toString() -> String {
+    return "[Person: firstName:\(firstName) lastName:\(lastName) age:\(age) job:\(job) spouse:\(spouse)]"
+  }
+}
+
+////////////////////////////////////
+// Family
 //
-//////////////////////////////////////
-//// Family
-////
-//public class Family {
-//  private var members : [Person] = []
-//  
-//  public init(spouse1: Person, spouse2: Person) {
-//  }
-//  
-//  public func haveChild(child: Person) -> Bool {
-//  }
-//  
-//  public func householdIncome() -> Int {
-//  }
-//}
-//
-//
-//
-//
-//
+public class Family {
+  private var members : [Person] = []
+  
+  public init(spouse1: Person, spouse2: Person) {
+    if spouse1.spouse == nil && spouse2.spouse == nil{
+        spouse1.spouse = spouse2
+        spouse2.spouse = spouse1
+        members = [spouse1, spouse2]
+    }
+  }
+  
+    public func haveChild(child: Person) -> Bool {
+        var check = false
+        for person in members {
+            if person.age > 21{
+                check = true
+            }
+        }
+        if check {
+            members += [child]
+        }
+    }
+  
+  public func householdIncome() -> Int {
+    var total = 0
+    for person in members {
+        if ((person.job?.salary) != nil) {
+            total += person.job?.salary
+        }
+    }
+  }
+}
+
+
+
+
+
